706ca90af74b0d54d6e23c4345c654a65850a26f
Utkarsh241100 <utkarshivnagar2007@gmail.com>
2023-12-27 19:23:30 +0530

    first commit

---
+++ Tic_Tac_Toe.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+int check(int box[][]) {
+    if((box[0][0]==box[0][1]==box[0][2])&&()) {
+
+    }
+}
+int main() {
+    int box[3][3];
+   do
+   {
+    
+   } while (check(box));
+   
+    return 0;
+}
